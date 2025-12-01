c21117dcf085d7fdae8ac6eebc67fd9e4feccb83
alvaroEspinozaMerida <espinozameridaal@gmail.com>
2025-12-01 12:35:56 -0800

    integrated frontend and backend application with modernUI styling

--- src/main/java/com/espinozameridaal/ui/ChatApp.java
+++ src/main/java/com/espinozameridaal/ui/ChatApp.java
@@ -26,57 +26,70 @@ public class ChatApp extends Application {
             System.exit(0);
         });
 
-//TEMP "LOGIN"
-        int port = 1234;
-        UserDao userDao = new UserDao();         
-        User currentUser;
+//        int port = 1234;
+//        UserDao userDao = new UserDao();
+//        User currentUser;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter username: ");
+//        String username = scanner.nextLine().trim();
+//
+//
+//        Client client = null;
+//
+//        try {
+//            currentUser = userDao.findOrCreateByUsername(username);
+//
+//            currentUser.friends = new ArrayList<>(userDao.getFriends(currentUser.userID));
+//
+//            System.out.println("Found / created user: " + currentUser.userName +
+//                    " (id " + currentUser.userID + ")");
+//        } catch (SQLException e) {
+//            System.out.println("Failed to connect to database. Exiting.");
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        try {
+//            Socket socket = new Socket("localhost", port);
+//            client = new Client(socket, currentUser, userDao);
+//
+//        } catch (Exception e) {
+//            System.out.println("Could not connect to server on port " + port);
+//            System.out.println("Try again when the server is up.");
+//            System.exit(0);
+//        }
+//
+//
+//        FXMLLoader loader = new FXMLLoader(
+//                getClass().getResource("main_menu.fxml")
+//        );
+//
+//
+//        Parent root = loader.load();
+//        MainMenuController controller = loader.getController();
+//        controller.init(client);
+//
+//        Scene scene = new Scene(root, 800, 600);
+//        scene.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm());
+//        stage.setTitle("Java TCP Chat");
+//        stage.setScene(scene);
+//        stage.show();
 
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Enter username: ");
-        String username = scanner.nextLine().trim();
-        
-//        Built off old version of the CLI program; need to make sure to add wornings as 
-//        pop ups instead of print statements 
-        Client client = null; 
-        
-        try {
-            currentUser = userDao.findOrCreateByUsername(username);
-
-            currentUser.friends = new ArrayList<>(userDao.getFriends(currentUser.userID));
-
-            System.out.println("Found / created user: " + currentUser.userName +
-                    " (id " + currentUser.userID + ")");
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to database. Exiting.");
-            e.printStackTrace();
-            return;
-        }
-
-        try {
-            Socket socket = new Socket("localhost", port);
-            client = new Client(socket, currentUser, userDao);
-            
-        } catch (Exception e) {
-            System.out.println("Could not connect to server on port " + port);
-            System.out.println("Try again when the server is up.");
-            System.exit(0);
-        }
-
-        
         FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("main_menu.fxml")
+                getClass().getResource("login_v2.fxml")
         );
-        
-        
         Parent root = loader.load();
-        MainMenuController controller = loader.getController();
-        controller.init(client);
-
-        Scene scene = new Scene(root, 800, 600);
-        stage.setTitle("Java TCP Chat");
+        Scene scene = new Scene(root, 1100, 700);
+        scene.getStylesheets().add(
+                getClass().getResource("css/style.css").toExternalForm()
+        );
+        stage.setTitle("Modern Chat - Login");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     // fix app not closing
